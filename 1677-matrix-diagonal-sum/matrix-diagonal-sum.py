87f3f6152e068e71dd3da67227a85dc5aa033cbf
SUJAYA KS <sujayaaks@gmail.com>
2026-09-09 17:27:05 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (42.52%) - LeetSync

---
+++ 1677-matrix-diagonal-sum/matrix-diagonal-sum.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def diagonalSum(self, mat):
+        n=len(mat)
+        s=0
+        for i in range(n):
+            s+=mat[i][i]+mat[i][n-i-1]
+        if n%2 !=0:
+            s-=mat[n//2][n//2]
+
+        return s
+        
