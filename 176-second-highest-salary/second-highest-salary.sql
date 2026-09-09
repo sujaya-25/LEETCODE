b3875e1522bfc28b876625758ee43c9c42125d28
SUJAYA KS <sujayaaks@gmail.com>
2026-09-09 17:36:01 +0530

    Time: 297 ms (59.10%) | Memory: 0B (100.00%) - LeetSync

---
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -0,0 +1,3 @@
+# Write your MySQL query statement below
+# Write your MySQL query statement below
+select distinct max(salary) as SecondHighestSalary from employee where salary<(select max(salary) from employee)
