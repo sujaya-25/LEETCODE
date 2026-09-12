1da4398fa97f733cf92769aeebee15579e96fbd3
SUJAYA KS <sujayaaks@gmail.com>
2026-09-12 15:33:18 +0530

    Time: 31 ms (57.18%) | Memory: 12.6 MB (17.72%) - LeetSync

---
+++ 657-robot-return-to-origin/robot-return-to-origin.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def judgeCircle(self, moves):
+        x = 0
+        y = 0
+
+        for i in moves:
+            if i == "U":
+                y += 1
+            elif i == "D":
+                y -= 1
+            elif i == "L":
+                x -= 1
+            else:
+                x += 1
+
+        return x == 0 and y == 0
