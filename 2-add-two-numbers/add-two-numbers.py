98d0c754126906161c449c2bcaf8a6c43aa5aa55
SUJAYA KS <sujayaaks@gmail.com>
2026-09-12 15:03:51 +0530

    Time: 3 ms (89.43%) | Memory: 12.5 MB (22.77%) - LeetSync

---
+++ 2-add-two-numbers/add-two-numbers.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        dummy = ListNode(0)
+        cur = dummy
+        carry = 0
+
+        while l1 or l2:
+            a = l1.val if l1 else 0
+            b = l2.val if l2 else 0
+
+            s = a + b + carry
+            carry = s // 10
+
+            cur.next = ListNode(s % 10)
+            cur = cur.next
+
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+
+        if carry:
+            cur.next = ListNode(carry)
+
+        return dummy.next
