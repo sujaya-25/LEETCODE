fa837a552cc41893bc9ab405da36566869c1c833
SUJAYA KS <sujayaaks@gmail.com>
2026-09-15 18:01:39 +0530

    Time: 3 ms (46.90%) | Memory: 12.5 MB (9.95%) - LeetSync

---
+++ 682-baseball-game/baseball-game.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def calPoints(self, operations):
+        a=[]
+        for i in operations:
+            if i =="C":
+                a.pop()
+            elif i=="D":
+                a.append(2*a[-1])
+            elif i=="+":
+                a.append(a[-1]+a[-2])
+            else:
+                a.append(int(i))
+        return sum(a)
+            
+                
+
+
+
+        
